95e7c16fcd8b902ce95a55150eeaab754d03dd74
jonathanhans22 <jonathanhans.2205@gmail.com>
2025-09-08 22:26:01 +0700

    tugas2

--- main/models.py
+++ main/models.py
@@ -8,18 +8,18 @@ class Product(models.Model):
         ('JERSEY', 'Jersey'),
         ('SHOES', 'Sepatu Bola'),
         ('BALL', 'Bola'),
-        ('ACCESSORIES', 'Aksesoris'),
-        ('APPAREL', 'Pakaian Latihan'),
+        ('BOTTLE', 'botol'),
+        ('', 'Pakaian Latihan'),
     ]
 
     name = models.CharField(max_length=200)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    # --- Gunakan choices yang sudah benar ---
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     is_featured = models.BooleanField(default=False)
     brand = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+    
