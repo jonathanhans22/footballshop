6ee2f0050a83fd535b997566acc1997e47b73ed3
jonathanhans22 <jonathanhans.2205@gmail.com>
2025-09-28 09:28:12 +0700

    tugas 5

--- main/views.py
+++ main/views.py
@@ -119,10 +119,10 @@ def login_user(request):
 
         if form.is_valid():
                 user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+                login(request, user)
+                response = HttpResponseRedirect(reverse("main:show_main"))
+                response.set_cookie('last_login', str(datetime.datetime.now()))
+                return response
 
     else:
         form = AuthenticationForm(request)
